c52a949e0600f292e534e2349b6cf2fdd4367bd6
Zachary Shuford <zshuford@freightsystemsinc.com>
2019-11-04 10:46:46 -0500

    Fixed classes so namespace and folder structure matched

--- ExtensionMethods/AreSameValue.cs
+++ ExtensionMethods/AreSameValue.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Poker
+namespace Poker.ExtensionMoethods
 {
     public static class AreSameValueExtension
     {
--- Factories/CardFactory.cs
+++ Factories/CardFactory.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Poker
+namespace Poker.Factories
 {
     public static class CardFactory
     {
